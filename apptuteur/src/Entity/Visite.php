a8d348088cb140c3c1beaa3af47356f42e1818cc
AdiyoFouad <odjfouad38@gmail.com>
2025-12-06 22:16:49 +0100

    Entity Tuteur & Visite

---
+++ apptuteur/src/Entity/Visite.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\VisiteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: VisiteRepository::class)]
+#[ApiResource]
+class Visite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE)]
+    #[Assert\NotBlank]
+    private ?\DateTimeImmutable $date = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(min: 5)]
+    private ?string $commentaire = null;
+
+    #[ORM\ManyToOne(inversedBy: 'visites')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Tuteur $tuteur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeImmutable $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): static
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getTuteur(): ?Tuteur
+    {
+        return $this->tuteur;
+    }
+
+    public function setTuteur(?Tuteur $tuteur): static
+    {
+        $this->tuteur = $tuteur;
+
+        return $this;
+    }
+}
